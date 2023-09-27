c32c931bfb1159fd8e1920f4b5b8f791571c4001
qwertyvc4 <rosayuliya@gmail.com>
2023-09-27 09:51:30 +0300

    test

---
+++ мдк3.1_пр4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace мдк3._1_пр4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
