42b09e3a05b3692edeaed93864d32cffb8f315a9
qwertyvc4 <135412875+qwertyvc4@users.noreply.github.com>
2023-10-04 16:29:22 +0300

    Add files via upload

---
+++ мдк3.1_пр4.1/мдк3.1_пр4.1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace мдк3._1_пр4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2; // первый элемент прогрессии
+            int r = 3; // знаменатель прогрессии
+            int n = 5; // количество элементов прогрессии
+
+            int result = a * (int)Math.Pow(r, n - 1);
+            int sum = (a * (int)Math.Pow(r, n) - a) / (r - 1);
+
+            Console.WriteLine("Последний элемент прогрессии: " + result);
+            Console.WriteLine("Сумма прогрессии: " + sum);
+        }
+        public static string Formatter(string message)
+        {
+            string res = string.Empty;
+            res += "-------------------------\n";
+            res += message + "\n";
+            res += "-------------------------\n";
+            return res;
+        }
+    }
+}
---
+++ мдк3.1_пр4/мдк3.1_пр4/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "CMakeList.txt";
+        string words = Console.ReadLine();
+        File.WriteAllText(filePath, words);     
+    }
+    public static string Formatter(string message)
+    {
+        string res = string.Empty;
+        res += "-------------------------\n";
+        res += message + "\n";
+        res += "-------------------------\n";
+        return res;
+    }
+}
